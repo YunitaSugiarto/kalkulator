879d66b27b0417aaa6e0c03a86826d69e110159d
Asus <Asus@LAPTOP-AVJ0VVHK>
2018-08-03 13:44:59 +0700

    Final Commit

--- src/kalkulator/Kalkulator.java
+++ src/kalkulator/Kalkulator.java
@@ -17,7 +17,22 @@ public class Kalkulator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       
+        double a ,b ,c , tugas;
+        Scanner masukan = new Scanner(System.in);
+        System.out.println("---Kalkulator Sederhana---");
+        System.out.println(" 1. Penjumlahan ");
+        System.out.println(" 2. Pengurangan ");
+        System.out.println(" 3. Perkalian ");
+        System.out.println(" 4. Pembagian ");
+        System.out.print("Pilih Tugas : ");   tugas = masukan.nextDouble();
+        System.out.print("Bilangan pertama : ");  a = masukan.nextDouble();
+        System.out.print("bilangan kedua : ");    b = masukan.nextDouble();
+        if (tugas == 1) c = a + b;
+        else if (tugas == 2) c = a - b;
+        else if (tugas == 3) c = a * b;
+        else if (tugas == 4) c = a / b;
+        else c = a % b;
+        System.out.println("Hasil = " + c);
     }
     
 }
